8d432ec7ba0a87c0c2550b76e0b203add1eb41c7
yoon <sin020511@gmail.com>
2025-04-30 21:48:01 +0900

    [FEAT] 결제 도메인 세팅

---
+++ src/main/java/com/example/demo/domain/dto/payment/ApproveResponse.java
@@ -0,0 +1,14 @@
+package com.example.demo.domain.dto.payment;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ApproveResponse {
+    @JsonProperty("approved_at")
+    private String approvedAt; // 결제 승인 시각
+}
---
+++ src/main/java/com/example/demo/domain/dto/payment/ReadyResponse.java
@@ -0,0 +1,22 @@
+package com.example.demo.domain.dto.payment;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ReadyResponse {
+    private String tid; // 결제 고유 번호
+
+    @JsonProperty("next_redirect_pc_url")
+    private String nextRedirectPcUrl; // PC용 결제 페이지 URL
+
+    @JsonProperty("next_redirect_mobile_url")
+    private String nextRedirectMobileUrl; // 모바일용 결제 페이지 URL
+
+    @JsonProperty("created_at")
+    private String createdAt; // 결제 생성 시각
+}
