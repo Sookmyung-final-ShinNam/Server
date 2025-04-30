5dedb5de60b388703cd9fff10a9459670ba4286e
yoon <sin020511@gmail.com>
2025-04-30 21:45:48 +0900

    [FEAT] 결제 api 세팅

---
+++ src/main/java/com/example/demo/repository/PaymentRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.base.payment.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    List<Payment> findByUserId(String userId);
+    Optional<Payment> findByTid(String tid);
+
+}
