04238001b9e07b11e8d70200a6e1b18e871bf812
yoon <sin020511@gmail.com>
2025-05-04 14:24:35 +0900

    [FIX] 불필요한 엔티티 제거

--- src/main/java/com/example/demo/repository/gpt/UserTextAnalysisRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository.gpt;
-
-import com.example.demo.entity.base.gpt.UserTextAnalysis;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserTextAnalysisRepository extends JpaRepository<UserTextAnalysis, Long> {
-}
